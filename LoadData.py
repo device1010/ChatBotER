b6e4210bdcbeac31c9b95962565b98338459f3db
GaliaGarnica <ga.gga4747@gmail.com>
2023-09-13 23:43:17 -0400

    Added Splitting

--- LoadData.py
+++ LoadData.py
@@ -3,20 +3,36 @@ import openai
 import sys
 from dotenv import load_dotenv, find_dotenv
 from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter  import RecursiveCharacterTextSplitter, CharacterTextSplitter
+
+
+def get_data_loaded():
+    pages = ""
+    for archivo in os.listdir("Resourcesp"):
+        if archivo.endswith(".pdf"):
+            ruta_pdf = os.path.join("Resourcesp", archivo)
+            loader = PyPDFLoader(ruta_pdf)
+            for page in loader.load():
+                pages+=page.page_content
+                #print(page.page_content)
+            #pages.append(loader.load())
+
+    return pages
+
+def get_chunks(data,chunk_size,chunk_overlap):
+    tr_splitter = RecursiveCharacterTextSplitter( chunk_size = chunk_size, chunk_overlap = chunk_overlap, length_function = len)
+    chunks = tr_splitter.split_text(data)
+    return chunks
+
 
 
 os.environ["OPENAI_API_KEY"] = "API-KEY"
 _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key = os.environ['OPENAI_API_KEY']
-
-
-pages = []
-for archivo in os.listdir("Resourcesp"):
-    if archivo.endswith(".pdf"):
-        
-        ruta_pdf = os.path.join("Resourcesp", archivo)
-        loader = PyPDFLoader(ruta_pdf)
-        pages.append(loader.load())
-        
-        
+ 
+data = get_data_loaded()
+#print(data)
+chunks = get_chunks(data,500,100)  #Send data, chunk size, chunk overlap, return chunks
+print(chunks)
 #print(pages[0][1].page_content[0:100])
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-u
+Link Resources 
+https://drive.google.com/drive/folders/1f58cMA2kDfcHVC4LjX7J0FcqZtd33yM6?usp=drive_link
